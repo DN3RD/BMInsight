15d88e437b13e2cec58b618152b3c44445d565a6
Abdelqoddouss JEBBAR <jebbarabdelqoddouss@gmail.com>
2025-03-09 22:09:04 +0100

    feat: add overlay component

--- src/app.rs
+++ src/app.rs
@@ -14,29 +14,62 @@ pub fn App() -> Element {
         link { rel: "stylesheet", href: "assets/styles.css" }
         link { rel: "stylesheet", href:"https://fonts.googleapis.com/icon?family=Material+Icons" }
         main {
-            class: "column container",
-            h2 {
-                class: "title",
-                "BMI Insight"
-            }
-            Gender {
-                gender: genderSignal
-            }
-            Ruler {
-                height: heightSignal,
-            }
-            Scale {
-                max: 1000,
-                min: 1,
-                title: "Weight (in Lb)",
-                scaleValue: weightSignal,
+            class: "main",
+            div {
+                class: "column container",
+                h2 {
+                    class: "title",
+                    "BMI Insight"
+                }
+                Gender {
+                    gender: genderSignal
+                }
+                Ruler {
+                    height: heightSignal,
+                }
+                Scale {
+                    max: 1000,
+                    min: 1,
+                    title: "Weight (in Lb)",
+                    scaleValue: weightSignal,
 
+                }
+                Scale {
+                    max: 100,
+                    min: 1,
+                    title: "Age",
+                    scaleValue: ageSignal,
+                }
             }
-            Scale {
-                max: 100,
-                min: 1,
-                title: "Age",
-                scaleValue: ageSignal,
+            div {
+                class: "result-container-wrapper",
+                div {
+                    class: "result-container",
+                    div {
+                        class: "circle",
+                        "BMI"
+                    }
+                    div {
+                        class: "result-wrapper hidden",
+                        p {
+                            class: "result-header",
+                            "Your BMI is"
+                        }
+                        p {
+                            class: "result-value",
+                            "19.6 Kg/m"
+                            sup { "2" }
+                        }
+                        p {
+                            class: "result-category",
+                            "(Normal)"
+                        }
+                        p {
+                            class: "result-recommendation",
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Sed sollicitudin erat eu augue interdum, in convallis libero feugiat. Morbi ac nulla sed felis iaculis luctus et at purus. Ut ut bibendum elit. Mauris iaculis, mauris et auctor consequat, nisl erat cursus metus, at iaculis felis lectus a erat. Proin ut libero et enim efficitur auctor. Sed et lacinia orci. Curabitur a odio mi."
+                        }
+                    }
+                }
             }
         }
     }
