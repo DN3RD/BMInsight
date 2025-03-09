cb552f86bd73cfa4258027cf419557d847a8e152
Abdelqoddouss JEBBAR <jebbarabdelqoddouss@gmail.com>
2025-03-09 02:52:11 +0100

    ruler scrollable

--- src/app.rs
+++ src/app.rs
@@ -1,78 +1,57 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
-use serde::{Deserialize, Serialize};
+use dioxus::web::WebEventExt;
+use web_sys::{console, HtmlElement, ScrollIntoViewOptions, ScrollToOptions};
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
-    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
-}
+fn format_height(height: u16) -> String {
+    let feet = height / 12;
+    let inches = height % 12;
 
-#[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
+    if inches == 0 {
+        format!("{}'", feet)
+    } else {
+        format!("{}'{}\"", feet, inches)
+    }
 }
 
 pub fn App() -> Element {
-    let mut name = use_signal(|| String::new());
-    let mut greet_msg = use_signal(|| String::new());
-
-
-    let greet = move |_: FormEvent| async move {
-        if name.read().is_empty() {
-            return;
+    let mut scroll_position = use_signal(|| 0f64);
+    let onscroll = move |event: Event<ScrollData>| {
+        let data = event.data();
+        if let Some(target) = event.data.as_web_event().target().and_then(|target| target.dyn_into::<HtmlElement>().ok()){
+            let scroll_index = target.scroll_left() as f64;
+            let options = ScrollToOptions::new();
+            let index: f64 = ((scroll_index - 12f64)/24f64).ceil();
+            options.set_behavior(web_sys::ScrollBehavior::Smooth);
+            options.set_left(index *24f64);
+            console::log_1(&format!("Scroll event scrollIndex: {:?}", scroll_index).into());
+            scroll_position.set(index);
         }
-
-        let name = name.read();
-        let args = serde_wasm_bindgen::to_value(&GreetArgs { name: &*name }).unwrap();
-        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-        let new_msg = invoke("greet", args).await.as_string().unwrap();
-        greet_msg.set(new_msg);
     };
-
     rsx! {
         link { rel: "stylesheet", href: "assets/styles.css" }
         main {
-            class: "container",
-            h1 { "Welcome to Tauri + Dioxus" }
-
+            class: "row container",
             div {
-                class: "row",
-                a {
-                    href: "https://tauri.app",
-                    target: "_blank",
-                    img {
-                        src: "assets/tauri.svg",
-                        class: "logo tauri",
-                         alt: "Tauri logo"
-                    }
-                }
-                a {
-                    href: "https://dioxuslabs.com/",
-                    target: "_blank",
-                    img {
-                        src: "assets/dioxus.png",
-                        class: "logo dioxus",
-                        alt: "Dioxus logo"
+                class: "row ruler",
+                onscroll: onscroll,
+                div {
+                    class: "row ruler-container",
+                    onscroll: onscroll,
+                    for (index,height) in (60..=96).enumerate() {
+                        div {
+                            class: if (index as f64 == scroll_position()) {"tick-container highlight"} else {"tick-container"},
+                            span {
+                                class: if height % 12 == 0 { "major-unit" } else { "minor-unit" },
+                                "{format_height(height)}"
+                            } // Interpolate the height in the p element
+                            div { class: if height % 12 == 0 { "major-tick" } else { "minor-tick" } }
+                        }
                     }
                 }
             }
-            p { "Click on the Tauri and Dioxus logos to learn more." }
-
-            form {
-                class: "row",
-                onsubmit: greet,
-                input {
-                    id: "greet-input",
-                    placeholder: "Enter a name...",
-                    value: "{name}",
-                    oninput: move |event| name.set(event.value())
-                }
-                button { r#type: "submit", "Greet" }
-            }
-            p { "{greet_msg}" }
         }
     }
 }
